d1abbfc3d33b65b04fb611b6ac0b61a7386de4c0
Jessenth Ebenezer <30726289+sm1else-bot@users.noreply.github.com>
2022-09-10 20:36:52 +0530

    files related to current project

---
+++ HW task/DestroyPulpit.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPulpit : MonoBehaviour
+{
+    public float interval;
+    //public float dtime = 2.0f;
+    //private float timer = 0.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (interval > 0){
+            interval -=Time.deltaTime;
+        }
+        else {
+            enabled=false;
+
+            gameObject.AddComponent<Rigidbody>();
+            Destroy(gameObject,2);
+            //timer+=Time.deltaTime;
+            //if(timer>dtime){
+            //    timer=0f;
+            //    Destroy(gameObject);
+            //}
+        }
+    }
+
+}
